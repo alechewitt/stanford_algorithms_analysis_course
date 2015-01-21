755b1b3e62a5f0181cbe07af6a3fa4d85d4feaef
Alec Hewitt <alec.hewitt@immerselearning.com>
2015-01-21 18:54:49 +0000

    Complete merge sort in go

--- 01_merge_sort/go_merge.go
+++ 01_merge_sort/go_merge.go
@@ -9,13 +9,34 @@ func mergeSort(mySlice[]int) []int{
 		return mySlice
 	} else {
 		// mySliceResult := make([]int, 0, 1000)
-		mySliceA := mySlice[:3]
-		// mySliceB := [3:6]
-		return mySliceA
+		halfLen := len(mySlice) / 2
+		mySliceA := mergeSort(mySlice[:halfLen])
+		mySliceB := mergeSort(mySlice[halfLen:])
+
+		mySliceResult := make([]int, 0, len(mySlice))
+		for len(mySliceA) > 0 || len(mySliceB) > 0 {
+			if len(mySliceA) == 0 {
+				mySliceResult = append(mySliceResult, mySliceB[0])
+				mySliceB = mySliceB[1:]
+			} else if len(mySliceB) == 0 {
+				mySliceResult = append(mySliceResult, mySliceA[0])
+				mySliceA = mySliceA[1:]
+			} else {
+				if mySliceA[0] > mySliceB[0] {
+					mySliceResult = append(mySliceResult, mySliceB[0])
+					mySliceB = mySliceB[1:]
+				} else {
+					mySliceResult = append(mySliceResult, mySliceA[0])
+					mySliceA = mySliceA[1:]
+				}
+			}
+
+		}
+		return mySliceResult
 	}	
 }
 
 func main() {
-	p := []int{2, 3, 5, 7, 11, 13}
+	p := []int{300, 200, 1, 2, 3, 5, 7, 11, 13, 1, 2}
 	fmt.Println(mergeSort(p))
 }
